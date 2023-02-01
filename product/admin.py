34fada250055fe8c8d9c779b8aee048983b7577b
Hanane-fallah <hananee.fallah@gmail.com>
2023-02-02 02:32:16 +0330

    [#010] add : promotion & promotion-type model

--- product/admin.py
+++ product/admin.py
@@ -4,3 +4,5 @@ from product.models import *
 
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Promotion)
+admin.site.register(PromotionType)
--- product/models.py
+++ product/models.py
@@ -1,6 +1,8 @@
+from datetime import date
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from user.models import expiry_date_validate
 
 
 # VALIDATORS
@@ -31,3 +33,22 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name} : {self.qty}'
+
+
+class PromotionType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Promotion(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    type_id = models.ForeignKey(PromotionType, on_delete=models.DO_NOTHING)
+    value = models.FloatField()
+    hint = models.CharField(max_length=50)  # in percentage -> maximum promotion & in cart promo -> Discount phrase
+    start_date = models.DateField(default=date.today)
+    end_date = models.DateField(null=False, validators=[expiry_date_validate])
+
+    def __str__(self):
+        return f'{self.name} : {self.value}'
