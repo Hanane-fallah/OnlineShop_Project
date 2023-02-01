49ac873c7025a952e1392c7b47c6c431d7de79af
Hanane-fallah <hananee.fallah@gmail.com>
2023-02-02 02:05:34 +0330

    [#009] add : product model

--- product/models.py
+++ product/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
 
 # VALIDATORS
+def digit_validate(digit):
+    if digit <= 0:
+        raise ValidationError(
+            _('must be positive')
+        )
 
 
 # MODELS
@@ -12,5 +20,14 @@ class Category(models.Model):
         return self.name
 
 
-# class Product(models.Model):
-#     name = models.CharField(max_length=100, unique=True)
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    category_id = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    brand = models.CharField(max_length=50)
+    info = models.TextField(max_length=300)
+    price = models.FloatField(validators=[digit_validate])
+    qty = models.IntegerField(validators=[digit_validate])
+    image = models.ImageField(upload_to='img/product')
+
+    def __str__(self):
+        return f'{self.name} : {self.qty}'
