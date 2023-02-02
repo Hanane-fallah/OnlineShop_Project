7d7756cb7a89d54d52212eb1f50554932937958b
Hanane-fallah <hananee.fallah@gmail.com>
2023-02-02 22:45:02 +0330

    [#021] test : app product model

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+
+class ModelsTestCase(TestCase):
+    def setUp(self) -> None:
+        a = Category.objects.create(name='test-cat')
+        Category.objects.create(parent_cat=a, name='test-child-cat')
+
+        b = Product.objects.create(
+            name='test-product',
+            category_id=a,
+            brand='test-brand',
+            info='some info',
+            price=2.2,
+            qty=10,
+            image='img/product/test.jpeg'
+        )
+
+        c = PromotionType.objects.create(name='discount')
+
+        d = Promotion.objects.create(
+            name='test-promo',
+            type_id=c,
+            value=20,
+            hint='28',
+            start_date='2023-02-02',
+            end_date='2023-02-04',
+        )
+
+        InProcessPromo.objects.create(product_id=b, promotion_id=d, in_process=True)
+
+        u = Customer.objects.create(first_name='test-name',
+                                    last_name='test-last',
+                                    user_name='test-user',
+                                    email='test@test.co',
+                                    age=24,
+                                    mobile='09909009009',
+                                    slug='test'
+                                    )  # id = 1
+
+        Review.objects.create(user_id=u, product_id=b, rating='3', comment='some comments')
+
+    def test_user_create(self):
+        cat_1 = Category.objects.get(name='test-cat')
+        cat_2 = Category.objects.get(name='test-child-cat')
+        product = Product.objects.get(name='test-product')
+        promo_type = PromotionType.objects.get(name='discount')
+        promo = Promotion.objects.get(name='test-promo')
+        inprocess_promo = InProcessPromo.objects.get(product_id=1, promotion_id=1, in_process=True)
+        review = Review.objects.get(user_id=1, product_id=1, rating='3', comment='some comments')
+        # print('ad ', promo_type)
+        # print('ad ', promo)
+        self.assertTrue(cat_1)
+        self.assertTrue(cat_2)
+        self.assertTrue(product)
+        self.assertTrue(promo_type)
+        self.assertTrue(promo)
+        self.assertTrue(inprocess_promo)
+        self.assertTrue(review)
