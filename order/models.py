945ee88bf2cf5f294c215238a95537cd38ff0dc3
Hanane-fallah <hananee.fallah@gmail.com>
2023-02-02 20:19:29 +0330

    [#013] add : usercart model (user shopping cart)

--- order/models.py
+++ order/models.py
@@ -1,3 +1,18 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from user.models import Customer
+
+
+# VALIDATORS
+
+# MODELS
+class UserCart(models.Model):
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4,
+                          editable=False)
+    user_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user_id} : {self.id}'
