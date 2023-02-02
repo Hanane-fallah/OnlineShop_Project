3279582dc1de8701717eb14fac5d81b7568f5e7e
Hanane-fallah <hananee.fallah@gmail.com>
2023-02-02 22:53:59 +0330

    [#022] modify : test app product

--- order/tests.py
+++ order/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+
+class ModelsTestCase(TestCase):
+    def setUp(self) -> None:
+        a = Customer.objects.create(first_name='test-name',
+                                    last_name='test-last',
+                                    user_name='test-user',
+                                    email='test@test.co',
+                                    age=24,
+                                    mobile='09909009009',
+                                    slug='test'
+                                    )  # id = 1
+
+        UserCart.objects.create(user_id=a)
+
+
+    def test_user_create(self):
+
+        user_cart = UserCart.objects.get(user_id=1)
+
+        self.assertTrue(user_cart)
