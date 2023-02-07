8ca32d9371a1385e2cf7b360a58388803d718055
Hanane-fallah <hananee.fallah@gmail.com>
2023-02-07 23:11:00 +0330

    [#032] add: customer change form

--- user/forms.py
+++ user/forms.py
@@ -1,9 +1,17 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, ReadOnlyPasswordHashField
 from .models import User
+from django import forms
 
 
 class CustomerCreationFrom(UserCreationForm):
-
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'mobile', 'age')
+
+
+class CustomerChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(help_text='You can change password <a href=\'../password/\'>here</a>')
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'password', 'email', 'mobile', 'age')
